51c96defc0e802d790d477f019696c9608b63cde
Jeddi212 <jedediahf51@gmail.com>
2022-06-05 15:55:44 +0700

    add new func

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Todo list cli using rust
 
 ![Jeddi's Profile Views](https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fgithub.com%2FJeddi212&countColor=%23fce775&style=flat-square)
+
+<hr>
+
+### Example usage
+`cargo run -- hello world`
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io::Read, hash::Hash};
 
 fn main() {
     // Input arguments from cli
@@ -39,5 +39,38 @@ impl Todo {
             content.push_str(&record);
         }
         std::fs::write("db.txt", content)
+    }
+
+    fn new() -> Result<Todo, std::io::Error>{
+        let mut f = std::fs::OpenOptions::new()
+            .write(true).create(true).read(true).open("db.txt")?;
+        
+        let mut content = String::new();
+        f.read_to_string(&mut content)?;
+
+        let map: HashMap<String, bool> = content
+            .lines()
+            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+            .map(|v| (v[0], v[1]))
+            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+            .collect();
+        Ok(Todo { map })
+
+        /* Another approcah
+            // allocate an empty HashMap
+            let mut map = HashMap::new();
+            
+            // loop over each lines of the file
+            for entries in content.lines() {
+                // split and bind values
+                let mut values = entries.split('\t');
+                let key = values.next().expect("No Key");
+                let val = values.next().expect("No Value");
+                // insert them into HashMap
+                map.insert(String::from(key), bool::from_str(val).unwrap());
+            }
+            // Return Ok
+            Ok(Todo { map })
+        */
     }
 }
